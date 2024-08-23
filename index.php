800ec2e7bfa59c5c08a6a9c9135e86c5a721f8b4
TahoTaho <TahoTaho@users.noreply.github.com>
2024-08-24 02:56:56 +0800

    Initial commit

--- index.php
+++ index.php
@@ -23,9 +23,9 @@
 
     <form action="uploaded.php" method="post" enctype="multipart/form-data">
         <div class="p-card">
-            <h3>PDF File</h3>
+            <h3>AUDIO File</h3>
             <p class="p-card__content">
-            <input type="file" name="pdf_file" accept=".pdf" />
+            <input type="file" name="audio_file" accept=".mp3" />
             </p>
         </div>
 
--- uploaded.php
+++ uploaded.php
@@ -9,15 +9,14 @@ if (!is_dir($upload_directory)) {
 }
 
 // Check if a file was uploaded
-if (isset($_FILES['pdf_file']) && $_FILES['pdf_file']['error'] === UPLOAD_ERR_OK) {
-    $uploaded_pdf_file = $upload_directory . basename($_FILES['pdf_file']['name']);
-    $temporary_file = $_FILES['pdf_file']['tmp_name'];
+if (isset($_FILES['audio_file']) && $_FILES['audio_file']['error'] === UPLOAD_ERR_OK) {
+    $uploaded_audio_file = $upload_directory . basename($_FILES['audio_file']['name']);
+    $temporary_file = $_FILES['audio_file']['tmp_name'];
 
     // Move the uploaded file
-    if (move_uploaded_file($temporary_file, $uploaded_pdf_file)) {
+    if (move_uploaded_file($temporary_file, $uploaded_audio_file)) {
         echo 'File uploaded successfully.<br>';
-        // Provide a link to the uploaded PDF file
-        echo '<a href="' . $relative_path . basename($_FILES['pdf_file']['name']) . '" target="_blank">View PDF</a>';
+        echo '<a href="' . $relative_path . basename($_FILES['audio_file']['name']) . '" target="_blank">Listen to MP3</a>';
     } else {
         echo 'Failed to upload file';
     }
